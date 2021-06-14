0a8d90f5e371677f46c07bcef4029171e91081d2
Judith Kahrer <judith@curity.io>
2021-06-14 11:55:39 +0200

    Move build from script to docker compose file

--- run.sh
+++ run.sh
@@ -10,26 +10,6 @@ if [ "$GATEWAY" != "nginx" ] && [ "$GATEWAY" != "kong" ]; then
 fi
 
 #
-# Build the custom Docker image for NGINX
-#
-docker build --no-cache -f ./reverse-proxy/nginx/Dockerfile -t custom_openresty:1.19.3.1-8-bionic .
-if [ $? -ne 0 ];
-then
-  echo "Docker NGINX build problem encountered"
-  exit 1
-fi
-
-#
-# Build the custom Docker image for Kong
-#
-docker build --no-cache -f ./reverse-proxy/kong/Dockerfile -t custom_kong:2.4.1 .
-if [ $? -ne 0 ];
-then
-  echo "Docker Kong build problem encountered"
-  exit 1
-fi
-
-#
 # Run ngrok so that we can call the local system over the internet from OAuth tools
 #
 kill -9 $(pgrep ngrok) 2>/dev/null
@@ -40,11 +20,10 @@ ngrok start --all &
 #
 if [ "$GATEWAY" == "nginx" ]; then
 
-  docker-compose --profile nginx up --force-recreate
-
+  docker-compose --profile nginx up --force-recreate --build
 elif [ "$GATEWAY" == "kong" ]; then
 
-  docker-compose --profile kong up --force-recreate
+  docker-compose --profile kong up --force-recreate --build
 fi
 if [ $? -ne 0 ];
 then
